36ff81d36e007fc81062f633101fec08a8127009
saurav <banerjee.saurav6@gmail.com>
2025-12-10 11:57:51 +0000

    datbase url update

--- utils/database.js
+++ utils/database.js
@@ -4,10 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
