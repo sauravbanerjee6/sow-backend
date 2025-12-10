0e8f175f02e0374d879499f14f0015e2c5f8ca34
saurav <banerjee.saurav6@gmail.com>
2025-12-10 11:44:10 +0000

    render database fix

--- utils/database.js
+++ utils/database.js
@@ -3,13 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
 const pool = new Pool({
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 5432
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+  ssl: { rejectUnauthorized: false },
 });
-
 
 export default pool;
